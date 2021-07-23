1059e1aa233d722ae4c59e3b6183febb5f897d7d
ngoc <ngoc@gmail.com>
2021-07-23 16:52:48 +0700

    uơdate 23/7.2021

---
+++ assets/views/register.js
@@ -0,0 +1,52 @@
+// $(function(){
+//     var $registerForm = $('#formregister');
+//     if ($registerForm.length){
+//         $registerForm.validate({
+//             rules: {
+//                 name:{
+//                     required: true
+//                 },
+//                 email:{
+//                     required: true
+//                 },
+//                 numberphone:{
+//                     required: true
+//                 },
+//                 password:{
+//                     required: true
+//                 },
+//                 passwordconfirm:{
+//                     required: true
+//                 },
+//             },
+
+//             messages: {
+//                 name: {
+//                     required: 'nhập tên đăng nhập tại đây!'
+
+//                 },
+//                 email: {
+//                     required: 'nhập email tại đây!'
+
+//                 },
+//                 numberphone: {
+//                     required: 'vui lòng điền số điện thoại!'
+
+//                 },
+//                 password: {
+//                     required: 'nhập mật khẩu!'
+
+//                 },
+//                 passwordconfirm: {
+//                     required: 'nhập lại mật khẩu!'
+
+//                 }
+                
+
+
+//             }
+//         })
+//     }
+// })
+
+
